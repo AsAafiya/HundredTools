31567f2f4fa20faf4de9cba04d18a9150fef8a17
Ishani <ishani6767soni@gmail.com>
2026-03-10 11:19:04 +0530

    merge conflict resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,10 @@ import './App.css'
 // import MergePdf from './components/tools/MergePdf.jsx'
 // import SplitPdf from './components/tools/SplitPdf.jsx'
 // import CompressPdf from './components/tools/CompressPdf.jsx'
+// import './App.css'
+import MergePdf from './components/tools/MergePdf.jsx'
+import SplitPdf from './components/tools/SplitPdf.jsx'
+import CompressPdf from './components/tools/CompressPdf.jsx'
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
@@ -15,4 +19,4 @@ function App() {
     </>
   )
 }
-export default App;
+export default App;                          
