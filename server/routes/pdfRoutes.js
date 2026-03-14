c3653ff50df6c3bb443abc0fad99ee5d2c39c7ed
Ishani <ishani6767soni@gmail.com>
2026-03-14 13:40:07 +0530

    Added compress PDF features

--- server/routes/pdfRoutes.js
+++ server/routes/pdfRoutes.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const upload = require("../middleware/uploadMiddleware");
-const { mergePDF } = require("../controllers/pdfController");
+// const { mergePDF } = require("../controllers/pdfController");
 
-router.post("/merge", upload.array("files"), mergePDF);
+// router.post("/merge", upload.array("files"), mergePDF);
+
+const {compressPDF}=require("../controllers/compressController");
+
+router.post("/compress", upload.single("file"),compressPDF);
 
 module.exports = router;
