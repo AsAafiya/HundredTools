f35ffb645fdf10dcdb260775248e53164235fe02
Puspendra Singh <puspendra02001@gmail.com>
2026-05-22 17:32:30 +0530

    update tools

--- client/src/services/pdfService.js
+++ client/src/services/pdfService.js
@@ -90,3 +90,28 @@ export const convertJpgToPdf = async (files) => {
 
   return response.data;
 };
+
+
+export const splitPDF = async (file) => {
+  const formData = new FormData();
+
+  formData.append("file", file);
+
+  const response = await api.post("/pdf/split", formData, {
+    responseType: "blob",
+  });
+
+  return response.data;
+};
+
+export const compressPDF = async (file) => {
+  const formData = new FormData();
+
+  formData.append("file", file);
+
+  const response = await api.post("/pdf/compress", formData, {
+    responseType: "blob",
+  });
+
+  return response.data;
+};
